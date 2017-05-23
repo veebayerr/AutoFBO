249c2cbe11989d29c32382dcb887964682fe1d01
veebayerr <veebayerr@gmail.com>
2017-05-23 17:27:03 -0400

    Initial commit

--- WeatherPolling.py
+++ WeatherPolling.py
@@ -1,2 +1,7 @@
 
+'''
+This class will handle all the communications with the sensors and will
+periodically store readings into an object to be passed to the main class.
+'''
+
 class WeatherPolling:
